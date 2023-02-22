7070e80ef8313b83b93bb206d7bc1257585d8bc1
Addie Pasok <addiepasok8@gmail.com>
2023-02-22 11:50:14 -0600

    fixed style and msw

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -5,10 +5,10 @@ import overwatch2 from './data/overwatch2.json'
 
 // This configures a Service Worker with the given request handlers.
 export const worker = setupWorker(
-  rest.get('https://www.freetogame.com/api/games', (req, res, ctx) => { // capture "GET /greeting" requests
+  rest.get('https://free-to-play-games-database.p.rapidapi.com/api/games', (req, res, ctx) => { // capture "GET /greeting" requests
     return res(ctx.json(multipleGames)) // respond using a mocked JSON body
   }),
-  rest.get('https://www.freetogame.com/api/game?id=452', (req, res, ctx) => { // capture "GET /greeting" requests
+  rest.get('https://free-to-play-games-database.p.rapidapi.com/api/game?id=540', (req, res, ctx) => { // capture "GET /greeting" requests
     return res(ctx.json(overwatch2)) // respond using a mocked JSON body
   })
 )
