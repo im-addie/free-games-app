c53e4e55204aa9f80e6c29bc0f6f166d46162160
Addie Pasok <addiepasok8@gmail.com>
2023-02-22 21:26:46 -0600

    created new-releases button; added home button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,15 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import GamesIcon from '@mui/icons-material/Games';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import NewReleasesContainer from '../NewReleases/NewReleasesContainer';
+import Homepage from '../Homepage/Homepage'
 
 // text for the buttons, would probably remove them
 const pages = ['Products', 'Pricing', 'Blog'];
@@ -53,53 +50,6 @@ function Navbar() {
           >
             free games finder
           </Typography>
-
-
-        {/* 
-        small screen collapsable navbar:
-        <div>
-           comment: nav menu on small screens
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            
-            comment: 3 lines icon on small screens that open small navbar
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              comment: buttons text for small screens
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </div>
-        */}
           
           {/* logo on small screens */}
           <GamesIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
@@ -128,24 +78,37 @@ function Navbar() {
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             
             {/* buttons text for big screens */}
+            <Link to='/' element={<Homepage/>}>
+              <Button
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                Home
+              </Button>
+            </Link>
+
               <Button
                 onClick={handleCloseNavMenu}
                 sx={{ my: 2, color: 'white', display: 'block' }}
               >
                 Category
               </Button>
+              
               <Button
                 onClick={handleCloseNavMenu}
                 sx={{ my: 2, color: 'white', display: 'block' }}
               >
                 Platform
               </Button>
-              <Button
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                New Releases
-              </Button>
+              
+              <Link to='/new-releases' element={<NewReleasesContainer/>}>
+                <Button
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  New Releases
+                </Button>
+              </Link>
           </Box>
 
         </Toolbar>
